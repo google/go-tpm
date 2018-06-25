baa1dd656164af8f43ef3ad4275a545262f36d3f
Andrew Lytvynov <awly@google.com>
2018-06-25 16:44:24 -0700

    Use default EK template if no NVRAM index is specified
    Default template is documented at https://trustedcomputinggroup.org/wp-content/uploads/Credential_Profile_EK_V2.0_R14_published.pdf

--- examples/tpm2-ekcert/main.go
+++ examples/tpm2-ekcert/main.go
@@ -2,11 +2,13 @@
 package main
 
 import (
+	"crypto"
 	"crypto/x509"
 	"errors"
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"math/big"
 	"os"
 	"reflect"
 
@@ -19,7 +21,7 @@ var (
 	// Default value is defined in section 7.8, "NV Memory" of the latest version pdf on:
 	// https://trustedcomputinggroup.org/resource/tcg-tpm-v2-0-provisioning-guidance/
 	certIndex = flag.Uint("cert-index", 0x01C00002, "NVRAM index of the certificate file")
-	tmplIndex = flag.Uint("template-index", 0x81010001, "NVRAM index of the EK template")
+	tmplIndex = flag.Uint("template-index", 0, "NVRAM index of the EK template; if empty, default RSA EK template is used")
 	outPath   = flag.String("output", "", "File path for output; leave blank to write to stdout")
 )
 
@@ -58,13 +60,47 @@ func readEKCert(path string, certIdx, tmplIdx uint32) ([]byte, error) {
 	}
 
 	// Initialize EK and compare public key to ekCert.PublicKey.
-	ekTemplate, err := tpm2.NVRead(rwc, tpmutil.Handle(tmplIdx))
-	if err != nil {
-		return nil, fmt.Errorf("reading EK template: %v", err)
-	}
-	ekh, ekPub, err := tpm2.CreatePrimaryRawTemplate(rwc, tpm2.HandleEndorsement, tpm2.PCRSelection{}, "", "", ekTemplate)
-	if err != nil {
-		return nil, fmt.Errorf("creating EK: %v", err)
+	var ekh tpmutil.Handle
+	var ekPub crypto.PublicKey
+	if tmplIdx != 0 {
+		ekTemplate, err := tpm2.NVRead(rwc, tpmutil.Handle(tmplIdx))
+		if err != nil {
+			return nil, fmt.Errorf("reading EK template: %v", err)
+		}
+		ekh, ekPub, err = tpm2.CreatePrimaryRawTemplate(rwc, tpm2.HandleEndorsement, tpm2.PCRSelection{}, "", "", ekTemplate)
+		if err != nil {
+			return nil, fmt.Errorf("creating EK: %v", err)
+		}
+	} else {
+		// Default EK template defined in:
+		// https://trustedcomputinggroup.org/wp-content/uploads/Credential_Profile_EK_V2.0_R14_published.pdf
+		ekh, ekPub, err = tpm2.CreatePrimary(rwc, tpm2.HandleEndorsement, tpm2.PCRSelection{}, "", "", tpm2.Public{
+			Type:    tpm2.AlgRSA,
+			NameAlg: tpm2.AlgSHA256,
+			Attributes: tpm2.FlagFixedTPM | tpm2.FlagFixedParent | tpm2.FlagSensitiveDataOrigin |
+				tpm2.FlagAdminWithPolicy | tpm2.FlagRestricted | tpm2.FlagDecrypt,
+			AuthPolicy: []byte{
+				0x83, 0x71, 0x97, 0x67, 0x44, 0x84,
+				0xB3, 0xF8, 0x1A, 0x90, 0xCC, 0x8D,
+				0x46, 0xA5, 0xD7, 0x24, 0xFD, 0x52,
+				0xD7, 0x6E, 0x06, 0x52, 0x0B, 0x64,
+				0xF2, 0xA1, 0xDA, 0x1B, 0x33, 0x14,
+				0x69, 0xAA,
+			},
+			RSAParameters: &tpm2.RSAParams{
+				Symmetric: &tpm2.SymScheme{
+					Alg:     tpm2.AlgAES,
+					KeyBits: 128,
+					Mode:    tpm2.AlgCFB,
+				},
+				KeyBits:  2048,
+				Exponent: 0,
+				Modulus:  big.NewInt(0),
+			},
+		})
+		if err != nil {
+			return nil, fmt.Errorf("creating EK: %v", err)
+		}
 	}
 	defer tpm2.FlushContext(rwc, ekh)
 
