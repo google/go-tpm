f40e53d1fb7311e4f006271e78afb1c974c39c0a
Joseph Richey <joerichey@google.com>
2018-06-27 08:41:30 -0700

    Clean up maxTPMResponse (#20)
    Moves explanatory comment into tpmutil package
    Removes unused variable from tpm package

--- tpmutil/run.go
+++ tpmutil/run.go
@@ -58,6 +58,10 @@ func OpenTPM(path string) (io.ReadWriteCloser, error) {
 	return rwc, nil
 }
 
+// maxTPMResponse is the largest possible response from the TPM. We need to know
+// this because we don't always know the length of the TPM response, and
+// /dev/tpm insists on giving it all back in a single value rather than
+// returning a header and a body in separate responses.
 const maxTPMResponse = 4096
 
 // RunCommand executes cmd with given tag and arguments. Returns TPM response
