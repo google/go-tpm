fe8fc5b2534d39ac88db15276aa03d933de87790
Joe Richey joerichey@google.com <joerichey@google.com>
2018-07-30 10:27:18 -0700

    Clarify errors and comments
    Make it clear what encodedTestCommand and expectedTestResponse are
    actually for. Make the errors conform to proper Go idioms.

--- tpmutil/tbs/tbs_windows_test.go
+++ tpmutil/tbs/tbs_windows_test.go
@@ -6,12 +6,12 @@ import (
 	"testing"
 )
 
-
 var (
-	// Encodes a call to Getrandom with a length of 0
-	encodedCommand = []byte{128, 1, 0, 0, 0, 12, 0, 0, 1, 123, 0, 0}
+	// Encodes a call to Getrandom() with a buffer of length zero, meaning
+	// this command effictivly does nothing.
+	encodedTestCommand = []byte{128, 1, 0, 0, 0, 12, 0, 0, 1, 123, 0, 0}
 	// Expected response buffer for the above command
-	expectedResponse = []byte{128, 1, 0, 0, 0, 12, 0, 0, 0, 0, 0, 0}
+	expectedTestResponse = []byte{128, 1, 0, 0, 0, 12, 0, 0, 0, 0, 0, 0}
 )
 
 func getContext(t *testing.T) Context {
@@ -37,7 +37,7 @@ func TestGetLogLargeBuffer(t *testing.T) {
 			return
 		}
 		if err != ErrInsufficientBuffer {
-			t.Fatalf("GetTCGLog failed: err = %v", err)
+			t.Fatalf("GetTCGLog failed: %v", err)
 		}
 		log = make([]byte, 2*len(log))
 	}
@@ -50,7 +50,7 @@ func TestGetLogWithNilSlice(t *testing.T) {
 
 	logLen, err := ctx.GetTCGLog(nil)
 	if err != nil {
-		t.Fatalf("First GetTCGLog failed: err = %v", err)
+		t.Fatalf("First GetTCGLog failed: %v", err)
 	}
 	if logLen == 0 {
 		t.Fatal("Expected positive TCGLog length")
@@ -58,11 +58,11 @@ func TestGetLogWithNilSlice(t *testing.T) {
 
 	log := make([]byte, logLen)
 	if _, err := ctx.GetTCGLog(log); err != nil {
-		t.Fatalf("Second GetTCGLog failed: err = %v", err)
+		t.Fatalf("Second GetTCGLog failed: %v", err)
 	}
 }
 
-// Make sure SubmitCommand can handle a nil command buffer.
+// SubmitCommand can handle a nil command buffer.
 func TestSubmitCommandNilCommand(t *testing.T) {
 	ctx := getContext(t)
 	defer ctx.Close()
@@ -70,45 +70,45 @@ func TestSubmitCommandNilCommand(t *testing.T) {
 	response := make([]byte, os.Getpagesize())
 	_, err := ctx.SubmitCommand(NormalPriority, nil, response)
 	if err != ErrBadParameter {
-		t.Fatalf("Expected ErrBadParameter from Submit Command: got %v", err)
+		t.Fatalf("SubmitCommand failed with %v: expected ErrBadParameter", err)
 	}
 }
 
-// Make sure SubmitCommand can handle a nil response buffer.
+// SubmitCommand can handle a nil response buffer.
 func TestSubmitCommandNilResponse(t *testing.T) {
 	ctx := getContext(t)
 	defer ctx.Close()
 
-	_, err := ctx.SubmitCommand(NormalPriority, encodedCommand, nil)
+	_, err := ctx.SubmitCommand(NormalPriority, encodedTestCommand, nil)
 	if err != ErrInvalidOutputPointer {
-		t.Fatalf("Expected ErrInvalidOutputPointer from Submit Command: got %v", err)
+		t.Fatalf("SubmitCommand failed with %v: expected ErrInvalidOutputPointer", err)
 	}
 }
 
-// Make sure SubmitCommand can handle a short response buffer.
+// SubmitCommand can handle a response buffer that is shorter than necessary.
 func TestSubmitCommandShortResponse(t *testing.T) {
 	ctx := getContext(t)
 	defer ctx.Close()
 
 	response := make([]byte, 1)
-	_, err := ctx.SubmitCommand(NormalPriority, encodedCommand, response)
+	_, err := ctx.SubmitCommand(NormalPriority, encodedTestCommand, response)
 	if err != ErrInsufficientBuffer {
-		t.Fatalf("Expected ErrInsufficientBuffer from Submit Command: got %v", err)
+		t.Fatalf("SubmitCommand failed with %v: expected ErrInsufficientBuffer", err)
 	}
 }
 
-// Make sure SubmitCommand can handle a long response buffer.
+// SubmitCommand can handle a response buffer that is longer than necessary.
 func TestSubmitCommandLongResponse(t *testing.T) {
 	ctx := getContext(t)
 	defer ctx.Close()
 
 	response := make([]byte, 100)
-	responseLen, err := ctx.SubmitCommand(NormalPriority, encodedCommand, response)
+	responseLen, err := ctx.SubmitCommand(NormalPriority, encodedTestCommand, response)
 	if err != nil {
-		t.Fatalf("SubmitCommand failed: err = %v", err)
+		t.Fatalf("SubmitCommand failed: %v", err)
 	}
 	response = response[:responseLen]
-	if !bytes.Equal(response, expectedResponse) {
-		t.Fatalf("Got response of %v, expected %v", response, expectedResponse)
+	if !bytes.Equal(response, expectedTestResponse) {
+		t.Fatalf("Got response of %v, expected %v", response, expectedTestResponse)
 	}
 }
