640aa9d35faeb07d12c0fff46a26ec9daed2ffc5
twitchy-jsonp <40675700+twitchy-jsonp@users.noreply.github.com>
2018-11-07 11:19:32 -0800

    Implement generation of blob for Credential Activation /w RSA EKs compliant with the TCG 2.0 EK Credential Profile (#44)
    * Implement Credential Activation blob generation, supporting RSA EKs compliant with the TCG 2.0 EK Credential Profile

--- tpm2/structures.go
+++ tpm2/structures.go
@@ -576,6 +576,24 @@ func (ci CertifyInfo) encode() ([]byte, error) {
 	return concat(n, qn)
 }
 
+// IDObject represents an encrypted credential bound to a TPM object.
+type IDObject struct {
+	IntegrityHMAC []byte
+	EncIdentity   []byte
+}
+
+// Encode packs the IDObject into a byte stream representing
+// a TPM2B_ID_OBJECT.
+func (o *IDObject) Encode() ([]byte, error) {
+	// encIdentity is packed raw, as the bytes representing the size
+	// of the credential value are present within the encrypted blob.
+	d, err := tpmutil.Pack(o.IntegrityHMAC, tpmutil.RawBytes(o.EncIdentity))
+	if err != nil {
+		return nil, fmt.Errorf("encoding IntegrityHMAC, EncIdentity: %v", err)
+	}
+	return tpmutil.Pack(d)
+}
+
 // CreationData describes the attributes and environment for an object created
 // on the TPM. This structure encodes/decodes to/from TPMS_CREATION_DATA.
 type CreationData struct {
@@ -686,7 +704,7 @@ func (n Name) encode() ([]byte, error) {
 			return nil, fmt.Errorf("encoding Handle: %v", err)
 		}
 	case n.Digest != nil:
-		if buf, err = n.Digest.encode(); err != nil {
+		if buf, err = n.Digest.Encode(); err != nil {
 			return nil, fmt.Errorf("encoding Digest: %v", err)
 		}
 	default:
@@ -739,7 +757,8 @@ func decodeHashValue(in *bytes.Buffer) (*HashValue, error) {
 	return &hv, nil
 }
 
-func (hv HashValue) encode() ([]byte, error) {
+// Encode represents the given hash value as a TPMT_HA structure.
+func (hv HashValue) Encode() ([]byte, error) {
 	return tpmutil.Pack(hv.Alg, tpmutil.RawBytes(hv.Value))
 }
 
