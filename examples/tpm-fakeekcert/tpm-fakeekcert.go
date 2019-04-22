4ec831d06ebac0ab27e7193cad2a2b70d8703eed
Tom D'Netto <jsonp@google.com>
2019-04-22 14:38:41 -0700

    Fix year in license header

--- examples/tpm-fakeekcert/tpm-fakeekcert.go
+++ examples/tpm-fakeekcert/tpm-fakeekcert.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2014, Google LLC All rights reserved.
+// Copyright (c) 2019, Google LLC All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
