d56f2edc3e9af7b1ee09b308985a04e779e316a1
akakou <akakou571@gmail.com>
2022-07-08 08:22:37 +0000

    Use TPM2_CreateLoaded on the tests of TPM2_Commit
    Update the test of TPM2_Commit to use TPM2_CreateLoaded
    instead of TPM2_Create/TPM2_Load.

--- direct/tpm2/commit_test.go
+++ direct/tpm2/commit_test.go
@@ -3,11 +3,9 @@ package tpm2
 import (
 	"testing"
 
-	"github.com/google/go-tpm/direct/helpers"
 	"github.com/google/go-tpm/direct/structures/tpm"
 	"github.com/google/go-tpm/direct/structures/tpm2b"
 	"github.com/google/go-tpm/direct/structures/tpma"
-	"github.com/google/go-tpm/direct/structures/tpml"
 	"github.com/google/go-tpm/direct/structures/tpms"
 	"github.com/google/go-tpm/direct/structures/tpmt"
 	"github.com/google/go-tpm/direct/structures/tpmu"
@@ -19,66 +17,20 @@ func TestCommit(t *testing.T) {
 	if err != nil {
 		t.Fatalf("could not connect to TPM simulator: %v", err)
 	}
-	defer thetpm.Close()
 
-	PCR7, err := CreatePCRSelection([]int{7})
-	if err != nil {
-		t.Fatalf("Failed to create PCRSelection")
-	}
+	password := []byte{1, 2, 3}
 
-	createPrimary := CreatePrimary{
-		PrimaryHandle: tpm.RHOwner,
-
-		InPublic: tpm2b.Public{
-			PublicArea: tpmt.Public{
-				Type:    tpm.AlgECC,
-				NameAlg: tpm.AlgSHA1,
-				ObjectAttributes: tpma.Object{
-					FixedTPM:            true,
-					FixedParent:         true,
-					SensitiveDataOrigin: true,
-					UserWithAuth:        true,
-					Decrypt:             true,
-					Restricted:          true,
-				},
-				Parameters: tpmu.PublicParms{
-					ECCDetail: &tpms.ECCParms{
-						Symmetric: tpmt.SymDefObject{
-							Algorithm: tpm.AlgAES,
-							KeyBits: tpmu.SymKeyBits{
-								AES: helpers.NewKeyBits(128),
-							},
-							Mode: tpmu.SymMode{
-								AES: helpers.NewAlgID(tpm.AlgCFB),
-							},
-						},
-						CurveID: tpm.ECCNistP256,
-					},
+	create := CreateLoaded{
+		ParentHandle: tpm.RHEndorsement,
+		InSensitive: tpm2b.SensitiveCreate{
+			Sensitive: tpms.SensitiveCreate{
+				UserAuth: tpm2b.Auth{
+					Buffer: password,
 				},
 			},
 		},
-		CreationPCR: tpml.PCRSelection{
-			PCRSelections: []tpms.PCRSelection{
-				{
-					Hash:      tpm.AlgSHA1,
-					PCRSelect: PCR7,
-				},
-			},
-		},
-	}
-
-	rspCP, err := createPrimary.Execute(thetpm)
-	if err != nil {
-		t.Fatalf("could not create primary key: %v", err)
-	}
-
-	create := Create{
-		ParentHandle: NamedHandle{
-			Handle: rspCP.ObjectHandle,
-			Name:   rspCP.Name,
-		},
-		InPublic: tpm2b.Public{
-			PublicArea: tpmt.Public{
+		InPublic: tpm2b.Template{
+			Template: tpmt.Public{
 				Type:    tpm.AlgECC,
 				NameAlg: tpm.AlgSHA1,
 				ObjectAttributes: tpma.Object{
@@ -112,12 +64,11 @@ func TestCommit(t *testing.T) {
 		},
 	}
 
-	_, err = create.Execute(thetpm)
+	rspC, err := create.Execute(thetpm)
 	if err != nil {
 		t.Fatalf("could not create key: %v", err)
 	}
 
-	flushContext := FlushContext{FlushHandle: rspCP.ObjectHandle}
+	flushContext := FlushContext{FlushHandle: rspC.ObjectHandle}
 	defer flushContext.Execute(thetpm)
-
 }
