2ae4bd92afe232203f5f38be3c3c8a69d31b01af
Joe Richey <joerichey@google.com>
2026-03-10 08:08:35 +0000

    Add tests for EK Policies A, B, and C
    The [EK Credential Profile](https://trustedcomputinggroup.org/tcg-ek-credential-profile-tpm-family-2-0)
    defines different EK auth policies for the "High Range" and "Low Range"
    EKs. These are denoted in that spec as: `PolicyA`, `PolicyB`, and
    `PolicyC`.
    
    This PR:
      - Verifies the computations of `PolicyA`, `PolicyB`, and `PolicyC` for
        SHA256, SHA384, and SHA512.
      - Checks that the currently exported templates (called `L-1`and
        `L-2` in the spec), have the expected auth values.
    
    A follow up PR will show how you can actually use a "High Range" EK
    with:
      - `TPM2_PolicySecret(TPM_RH_ENDORSEMENT)`
      - `TPM2_PolicyAuthorizeNV()`
      - A normal auth _value_ (as opposed to a policy)
    
    Signed-off-by: Joe Richey <joerichey@google.com>

--- tpm2/test/ek_test.go
+++ tpm2/test/ek_test.go
@@ -1,6 +1,8 @@
 package tpm2test
 
 import (
+	"bytes"
+	"encoding/hex"
 	"errors"
 	"testing"
 
@@ -8,6 +10,223 @@ import (
 	"github.com/google/go-tpm/tpm2/transport"
 	"github.com/google/go-tpm/tpm2/transport/simulator"
 )
+
+// Decodes the provided hex strings into a byte array. Panics on non-hex chars.
+func hexToBytes(hexStrings ...string) []byte {
+	buf, err := []byte{}, error(nil)
+	for _, s := range hexStrings {
+		buf, err = hex.AppendDecode(buf, []byte(s))
+		if err != nil {
+			panic(err)
+		}
+	}
+	return buf
+}
+
+// Values for the various EK Policies and Templates specified in:
+// TCG EK Credential Profile For TPM Family 2.0; Level 0, Version 2.7
+var (
+	// PolicyA values from "Computing PolicyA" section
+	PolicyA = map[TPMIAlgHash]TPM2BDigest{
+		TPMAlgSHA256: {Buffer: hexToBytes(
+			"837197674484b3f81a90cc8d46a5d724",
+			"fd52d76e06520b64f2a1da1b331469aa",
+		)},
+		TPMAlgSHA384: {Buffer: hexToBytes(
+			"8bbf2266537c171cb56e403c4dc1d4b6",
+			"4f432611dc386e6f532050c3278c930e",
+			"143e8bb1133824ccb431053871c6db53",
+		)},
+		TPMAlgSHA512: {Buffer: hexToBytes(
+			"1e3b76502c8a1425aa0b7b3fc646a1b0",
+			"fae063b03b5368f9c4cddecaff0891dd",
+			"682bac1a85d4d832b781ea451915de5f",
+			"c5bf0dc4a1917cd42fa041e3f998e0ee",
+		)},
+	}
+	// Policy NV Indices from "Handle Values" section
+	PolicyIndex = map[TPMIAlgHash]TPMIRHNVIndex{
+		TPMAlgSHA256: 0x01C07F01,
+		TPMAlgSHA384: 0x01C07F02,
+		TPMAlgSHA512: 0x01C07F03,
+	}
+	// Policy Index Names from "Computing Policy Index Names" section
+	PolicyIndexName = map[TPMIAlgHash]TPM2BName{
+		TPMAlgSHA256: {Buffer: hexToBytes(
+			"000b", // TPM_ALG_SHA256
+			"0c9d717e9c3fe69fda41769450bb1459",
+			"57f8b3610e084dbf65591a5d11ecd83f",
+		)},
+		TPMAlgSHA384: {Buffer: hexToBytes(
+			"000c", // TPM_ALG_SHA384
+			"db62fca346612c976732ff4e8621fb4e",
+			"858be82586486504f7d02e621f8d7d61",
+			"ae32cfc60c4d120609ed6768afcf090c",
+		)},
+		TPMAlgSHA512: {Buffer: hexToBytes(
+			"000d", // TPM_ALG_SHA512
+			"1c47c0bbcbd3cf7d7cae6987d31937c1",
+			"71015dde3b7f0d3c869bca1f7e8a223b",
+			"9acfadb49b7c9cf14d450f41e9327de3",
+			"4d9291eece2c58ab1dc10e9059cce560",
+		)},
+	}
+	// PolicyC values from "Computing PolicyC" section
+	PolicyC = map[TPMIAlgHash]TPM2BDigest{
+		TPMAlgSHA256: {Buffer: hexToBytes(
+			"3767e2edd43ff45a3a7e1eaefcef7864",
+			"3dca964632e7aad82c673a30d8633fde",
+		)},
+		TPMAlgSHA384: {Buffer: hexToBytes(
+			"d6032ce61f2fb3c240eb3cf6a33237ef",
+			"2b6a16f4293c22b455e261cffd217ad5",
+			"b4947c2d73e63005eed2dc2b3593d165",
+		)},
+		TPMAlgSHA512: {Buffer: hexToBytes(
+			"589ee1e146544716e8deafe6db247b01",
+			"b81e9f9c7dd16b814aa159138749105f",
+			"ba5388dd1dea702f35240c184933121e",
+			"2c61b8f50d3ef91393a49a38c3f73fc8",
+		)},
+	}
+	// PolicyB values from "Computing PolicyB" section
+	PolicyB = map[TPMIAlgHash]TPM2BDigest{
+		TPMAlgSHA256: {Buffer: hexToBytes(
+			"ca3d0a99a2b93906f7a3342414efcfb3",
+			"a385d44cd1fd459089d19b5071c0b7a0",
+		)},
+		TPMAlgSHA384: {Buffer: hexToBytes(
+			"b26e7d28d11a50bc53d882bcf5fd3a1a",
+			"074148bb35d3b4e4cb1c0ad9bde419ca",
+			"cb47ba09699646150f9fc000f3f80e12",
+		)},
+		TPMAlgSHA512: {Buffer: hexToBytes(
+			"b8221ca69e8550a4914de3faa6a18c07",
+			"2cc01208073a928d5d66d59ef79e49a4",
+			"29c41a6b269571d57edb25fbdb183842",
+			"5608b413cd616a5f6db5b6071af99bea",
+		)},
+	}
+)
+
+// Test that PolicyCalculator correctly computes PolicyA for all hashes.
+func TestCalculatePolicyA(t *testing.T) {
+	// PolicyA only makes use of TPM2_PolicySecret(TPM_RH_ENDORSEMENT).
+	policySecretCmd := PolicySecret{
+		AuthHandle: TPMRHEndorsement,
+	}
+
+	for alg, policy := range PolicyA {
+		hash, err := alg.Hash()
+		if err != nil {
+			t.Fatalf("%v", err)
+		}
+		t.Run(hash.String(), func(t *testing.T) {
+			pol, err := NewPolicyCalculator(alg)
+			if err != nil {
+				t.Fatalf("creating policy calculator: %v", err)
+			}
+			if err = policySecretCmd.Update(pol); err != nil {
+				t.Fatalf("error updating policy calculator: %v", err)
+			}
+			digest := pol.Hash().Digest
+			if !bytes.Equal(digest, policy.Buffer) {
+				t.Errorf("PolicyA = %x,\nwant %x", digest, policy.Buffer)
+			}
+		})
+	}
+}
+
+// Test our Name calculation for the Policy NV Index (part of PolicyC).
+func TestCalculatePolicyIndexName(t *testing.T) {
+	for alg, name := range PolicyIndexName {
+		hash, err := alg.Hash()
+		if err != nil {
+			t.Fatalf("%v", err)
+		}
+		t.Run(hash.String(), func(t *testing.T) {
+			nvPub := TPMSNVPublic{
+				NVIndex: PolicyIndex[alg],
+				NameAlg: alg,
+				Attributes: TPMANV{
+					PolicyWrite: true,
+					WriteAll:    true,
+					PPRead:      true,
+					OwnerRead:   true,
+					AuthRead:    true,
+					PolicyRead:  true,
+					NoDA:        true,
+					Written:     true,
+				},
+				AuthPolicy: PolicyA[alg],
+				DataSize:   uint16(hash.Size() + 2),
+			}
+			nvName, err := NVName(&nvPub)
+			if err != nil {
+				t.Fatalf("computing NV Name: %v", err)
+			}
+			if !bytes.Equal(nvName.Buffer, name.Buffer) {
+				t.Errorf("NVName = %x,\nwant %x", nvName.Buffer, name.Buffer)
+			}
+		})
+	}
+}
+
+// Test that PolicyCalculator correctly computes PolicyC for all hashes.
+func TestCalculatePolicyC(t *testing.T) {
+	for alg, policy := range PolicyC {
+		hash, err := alg.Hash()
+		if err != nil {
+			t.Fatalf("%v", err)
+		}
+		t.Run(hash.String(), func(t *testing.T) {
+			pol, err := NewPolicyCalculator(alg)
+			if err != nil {
+				t.Fatalf("creating policy calculator: %v", err)
+			}
+			// PolicyC uses TPM2_PolicyAuthorizeNV(idx) to delegate policy.
+			authNVCmd := PolicyAuthorizeNV{
+				NVIndex: NamedHandle{
+					Handle: PolicyIndex[alg],
+					Name:   PolicyIndexName[alg],
+				},
+			}
+			if err = authNVCmd.Update(pol); err != nil {
+				t.Fatalf("error updating policy calculator: %v", err)
+			}
+			digest := pol.Hash().Digest
+			if !bytes.Equal(digest, policy.Buffer) {
+				t.Errorf("PolicyC = %x,\nwant %x", digest, policy.Buffer)
+			}
+		})
+	}
+}
+
+// Test that PolicyCalculator correctly computes PolicyB for all hashes.
+func TestCalculatePolicyB(t *testing.T) {
+	for alg, policy := range PolicyB {
+		hash, err := alg.Hash()
+		if err != nil {
+			t.Fatalf("%v", err)
+		}
+		t.Run(hash.String(), func(t *testing.T) {
+			pol, err := NewPolicyCalculator(alg)
+			if err != nil {
+				t.Fatalf("creating policy calculator: %v", err)
+			}
+			// PolicyB is just the TPM2_PolicyOR of PolicyA and PolicyC.
+			digests := []TPM2BDigest{PolicyA[alg], PolicyC[alg]}
+			orCmd := PolicyOr{PHashList: TPMLDigest{Digests: digests}}
+			if err = orCmd.Update(pol); err != nil {
+				t.Fatalf("error updating policy calculator: %v", err)
+			}
+			digest := pol.Hash().Digest
+			if !bytes.Equal(digest, policy.Buffer) {
+				t.Errorf("PolicyC = %x,\nwant %x", digest, policy.Buffer)
+			}
+		})
+	}
+}
 
 // Test creating a sealed data blob on the standard-template EK using its policy.
 func TestEKPolicy(t *testing.T) {
@@ -36,6 +255,13 @@ func ekPolicy(t transport.TPM, handle TPMISHPolicy, nonceTPM TPM2BNonce) error {
 
 // This function tests a lot of combinations of authorizing the EK policy.
 func ekTest(t *testing.T, ekTemplate TPMTPublic) {
+	// Before using the EK, ensure it has the expected policy.
+	policy := ekTemplate.AuthPolicy
+	expected := PolicyA[ekTemplate.NameAlg]
+	if !bytes.Equal(policy.Buffer, expected.Buffer) {
+		t.Errorf("AuthPolicy = %x,\nwant %x", policy.Buffer, expected.Buffer)
+	}
+
 	type ekTestCase struct {
 		name string
 		// Use Policy instead of PolicySession, passing the callback instead of
